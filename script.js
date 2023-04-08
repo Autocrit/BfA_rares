876db691ff32f1d5b629b72945ee9ec06e978b8c
Autocrit <Autocrit@users.noreply.github.com>
2023-04-08 16:50:41 +0100

    Squashed commit of the following:
    commit 624a59d4e72e0d12430db596a392ac1bad616d19
    Author: Autocrit <Autocrit@users.noreply.github.com>
    Date:   Sat Apr 8 15:48:01 2023 +0100
    
        Update styles.css
    
    commit 58e9b2b7954404689b6c69f7782cb80c56f288f8
    Author: Autocrit <Autocrit@users.noreply.github.com>
    Date:   Sat Apr 8 13:11:22 2023 +0100
    
        Fix for wowhead tooltips background
    
    commit e2df9f79a7bd4b4e232359da663e12f94ae5ce6b
    Author: Autocrit <Autocrit@users.noreply.github.com>
    Date:   Sat Apr 8 07:59:30 2023 +0100
    
        Update script.js
    
        Fix for async classes. Better sort
    
    commit 1e7453777933bcc08f29051936f7d58696dcf651
    Author: Autocrit <Autocrit@users.noreply.github.com>
    Date:   Fri Apr 7 21:57:11 2023 +0100
    
        Vol'dun and Stormsong Valley rares
    
        Vol'dun and Stormsong Valley rares and other improvements

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
+// Slot sort order
+const slots = [
+	"Head",
+	"Shoulder",
+	"Chest",
+	"Wrist",
+	"Hands",
+	"Waist",
+	"Legs",
+	"Feet",
+	"Neck",
+	"Back",
+	"Finger",
+	"Trinket",
+	"One-Hand",
+	"Two-Hand",
+	"Ranged",
+	"Shield",
+	"Held In Off-hand"
+];
+
 window.onload = function onLoad()
 {
 	updateClass();
 }
 
 function updateItems() {
-	var tbody = document.getElementById("tbody1");
+	var tbody = document.getElementById("table_contents");
 
 	// Clear table body
 	while(tbody.hasChildNodes()) {
@@ -18,66 +39,102 @@ function updateItems() {
 	fetch('./items.json')
 		.then(res => res.json())
 		.then(items => {
-			items.forEach(item => {
-				if((!document.getElementById("drustvar").checked && item.zone == "Drustvar") || 
-					(!document.getElementById("nazmir").checked && item.zone == "Nazmir") ||
-					(!document.getElementById("tiragarde_sound").checked && item.zone == "Tiragarde Sound") ||
-					(!document.getElementById("zuldazar").checked && item.zone == "Zuldazar") ||
-					(!document.getElementById("cloth_armour").checked && item.type == "Cloth armour") ||
-					(!document.getElementById("leather_armour").checked && item.type == "Leather armour") ||
-					(!document.getElementById("mail_armour").checked && item.type == "Mail armour") ||
-					(!document.getElementById("plate_armour").checked && item.type == "Plate armour") ||
-					(!document.getElementById("agility").checked && item.primaryAttribute == "Agility") ||
-					(!document.getElementById("intellect").checked && item.primaryAttribute == "Intellect") ||
-					(!document.getElementById("strength").checked && item.primaryAttribute == "Strength") ||
-					(!document.getElementById("wrist").checked && item.slot == "Wrist") ||
-					(!document.getElementById("hands").checked && item.slot == "Hands") ||
-					(!document.getElementById("waist").checked && item.slot == "Waist") ||
-					(!document.getElementById("legs").checked && item.slot == "Legs") ||
-					(!document.getElementById("feet").checked && item.slot == "Feet") ||
-					(!document.getElementById("cloak").checked && item.type == "Cloak") ||
-					(!document.getElementById("ring").checked && item.type == "Ring") ||
-					(!document.getElementById("trinket").checked && item.type == "Trinket") ||
-					(!document.getElementById("dagger").checked && item.type == "Dagger") ||
-					(!document.getElementById("one_handed_axe").checked && item.type == "One-Handed Axe") ||
-					(!document.getElementById("one_handed_mace").checked && item.type == "One-Handed Mace") ||
-					(!document.getElementById("one_handed_sword").checked && item.type == "One-Handed Sword") ||
-					(!document.getElementById("fist_weapon").checked && item.type == "Fist Weapon") ||
-					(!document.getElementById("wand").checked && item.type == "Wand") ||
-					(!document.getElementById("warglaives").checked && item.type == "Warglaives") ||
-					(!document.getElementById("polearm").checked && item.type == "Polearm") ||
-					(!document.getElementById("staff").checked && item.type == "Staff") ||
-					(!document.getElementById("two_handed_axe").checked && item.type == "Two-Handed Axe") ||
-					(!document.getElementById("two_handed_mace").checked && item.type == "Two-Handed Mace") ||
-					(!document.getElementById("two_handed_sword").checked && item.type == "Two-Handed Sword") ||
-					(!document.getElementById("bow").checked && item.type == "Bow") ||
-					(!document.getElementById("crossbow").checked && item.type == "Crossbow") ||
-					(!document.getElementById("gun").checked && item.type == "Gun") ||
-					(!document.getElementById("shield").checked && item.type == "Shield") ||
-					(!document.getElementById("off_hand_frill").checked && item.type == "Off-hand Frill") ||
-					(item.exclude)) {
-						return;
-				}
+		// Filter items based on options
+		var filteredItems = [];
+		items.forEach(item => {
+			if((!document.getElementById("drustvar").checked && item.zone == "Drustvar") || 
+				(!document.getElementById("nazmir").checked && item.zone == "Nazmir") ||
+				(!document.getElementById("tiragarde_sound").checked && item.zone == "Tiragarde Sound") ||
+				(!document.getElementById("zuldazar").checked && item.zone == "Zuldazar") ||
+				(!document.getElementById("vol_dun").checked && item.zone == "Vol'dun") ||
+				(!document.getElementById("stormsong_valley").checked && item.zone == "Stormsong Valley") ||
+				(!document.getElementById("cloth_armor").checked && item.type == "Cloth Armor") ||
+				(!document.getElementById("leather_armor").checked && item.type == "Leather Armor") ||
+				(!document.getElementById("mail_armor").checked && item.type == "Mail Armor") ||
+				(!document.getElementById("plate_armor").checked && item.type == "Plate Armor") ||
+				(!document.getElementById("agility").checked && item.primaryAttribute == "Agility") ||
+				(!document.getElementById("intellect").checked && item.primaryAttribute == "Intellect") ||
+				(!document.getElementById("strength").checked && item.primaryAttribute == "Strength") ||
+				(!document.getElementById("wrist").checked && item.slot == "Wrist") ||
+				(!document.getElementById("hands").checked && item.slot == "Hands") ||
+				(!document.getElementById("waist").checked && item.slot == "Waist") ||
+				(!document.getElementById("legs").checked && item.slot == "Legs") ||
+				(!document.getElementById("feet").checked && item.slot == "Feet") ||
+				(!document.getElementById("back").checked && item.slot == "Back") ||
+				(!document.getElementById("finger").checked && item.slot == "Finger") ||
+				(!document.getElementById("trinket").checked && item.slot == "Trinket") ||
+				(!document.getElementById("dagger").checked && item.type == "Dagger") ||
+				(!document.getElementById("one_handed_axe").checked && item.type == "One-Handed Axe") ||
+				(!document.getElementById("one_handed_mace").checked && item.type == "One-Handed Mace") ||
+				(!document.getElementById("one_handed_sword").checked && item.type == "One-Handed Sword") ||
+				(!document.getElementById("fist_weapon").checked && item.type == "Fist Weapon") ||
+				(!document.getElementById("wand").checked && item.type == "Wand") ||
+				(!document.getElementById("warglaives").checked && item.type == "Warglaives") ||
+				(!document.getElementById("polearm").checked && item.type == "Polearm") ||
+				(!document.getElementById("staff").checked && item.type == "Staff") ||
+				(!document.getElementById("two_handed_axe").checked && item.type == "Two-Handed Axe") ||
+				(!document.getElementById("two_handed_mace").checked && item.type == "Two-Handed Mace") ||
+				(!document.getElementById("two_handed_sword").checked && item.type == "Two-Handed Sword") ||
+				(!document.getElementById("bow").checked && item.type == "Bow") ||
+				(!document.getElementById("crossbow").checked && item.type == "Crossbow") ||
+				(!document.getElementById("gun").checked && item.type == "Gun") ||
+				(!document.getElementById("shield").checked && item.type == "Shield") ||
+				(!document.getElementById("off_hand_frill").checked && item.type == "Off-hand Frill") ||
+				(item.exclude)) {
+					return;
+			}
 
-				var row = tbody.insertRow();
-				var cell = row.insertCell();
-				cell.innerHTML = "<a href=\"" + item.itemLink + "\">" + item.name + "</a>";
-				cell = row.insertCell();
-				cell.textContent = item.slot;
-				cell = row.insertCell();
-				cell.textContent = item.type;
-				cell = row.insertCell();
-				cell.innerHTML = "<a href=\"" + item.sourceLink + "\">" + item.source + "</a>";
-				cell = row.insertCell();
-				cell.textContent = item.zone;
-				cell = row.insertCell();
-				cell.textContent = item.tomtomCommand;
-				cell = row.insertCell();
-				cell.textContent = item.primaryAttribute;
-
-				document.getElementById("waypoints").innerHTML += item.tomtomCommand + "<br>";
-			});
+			filteredItems.push(item);
 		});
+
+		// Sort by slot
+		filteredItems.sort(function(a, b) {
+			return slots.indexOf(a.slot) - slots.indexOf(b.slot);
+		});
+
+		// Add items to table
+		filteredItems.forEach(item => {
+			var row = tbody.insertRow();
+			var cell = null, anchor = null;
+
+			// Name
+			anchor = document.createElement("a");
+			anchor.setAttribute("href", item.itemURL);
+			anchor.textContent = item.name;
+			cell = row.insertCell();
+			cell.appendChild(anchor);
+
+			// Slot
+			cell = row.insertCell();
+			cell.textContent = item.slot;
+
+			// Type
+			cell = row.insertCell();
+			cell.textContent = item.type;
+
+			// Source
+			cell = row.insertCell();
+			anchor = document.createElement("a");
+			anchor.setAttribute("href", item.sourceURL);
+			anchor.textContent = item.source;
+			cell.appendChild(anchor);
+
+			// Zone
+			cell = row.insertCell();
+			cell.textContent = item.zone;
+
+			// TomTom command
+			cell = row.insertCell();
+			cell.textContent = item.tomtomCommand;
+
+			// Primary attribute
+			cell = row.insertCell();
+			cell.textContent = item.primaryAttribute;
+
+			// Add to list off waypoints
+			document.getElementById("waypoints").innerHTML += item.tomtomCommand + "<br>";
+		});
+	});
 }
 
 function updateClass() {
@@ -90,10 +147,10 @@ function updateClass() {
 		}
 	}
 
-	document.getElementById("cloth_armour").checked = false;
-	document.getElementById("leather_armour").checked = false;
-	document.getElementById("mail_armour").checked = false;
-	document.getElementById("plate_armour").checked = false;
+	document.getElementById("cloth_armor").checked = false;
+	document.getElementById("leather_armor").checked = false;
+	document.getElementById("mail_armor").checked = false;
+	document.getElementById("plate_armor").checked = false;
 	document.getElementById("agility").checked = false;
 	document.getElementById("intellect").checked = false;
 	document.getElementById("strength").checked = false;
@@ -119,59 +176,59 @@ function updateClass() {
 	fetch('./classes.json')
 		.then(res => res.json())
 		.then(classes => {
-			classes.forEach(wowClass => {
-				if(wowClass.class == strClass) {
-					if(wowClass.cloth)
-						document.getElementById("cloth_armour").checked = true;
-					if(wowClass.leather)
-						document.getElementById("leather_armour").checked = true;
-					if(wowClass.mail)
-						document.getElementById("mail_armour").checked = true;
-					if(wowClass.plate)
-						document.getElementById("plate_armour").checked = true;
-					if(wowClass.agility)
-						document.getElementById("agility").checked = true;
-					if(wowClass.intellect)
-						document.getElementById("intellect").checked = true;
-					if(wowClass.strength)
-						document.getElementById("strength").checked = true;
-					if(wowClass.dagger)
-						document.getElementById("dagger").checked = true;
-					if(wowClass.one_handed_axe)
-						document.getElementById("one_handed_axe").checked = true;
-					if(wowClass.one_handed_mace)
-						document.getElementById("one_handed_mace").checked = true;
-					if(wowClass.one_handed_sword)
-						document.getElementById("one_handed_sword").checked = true;
-					if(wowClass.fist_weapon)
-						document.getElementById("fist_weapon").checked = true;
-					if(wowClass.wand)	
-						document.getElementById("wand").checked = true;
-					if(wowClass.warglaives)
-						document.getElementById("warglaives").checked = true;
-					if(wowClass.polearm)
-						document.getElementById("polearm").checked = true;
-					if(wowClass.staff)
-						document.getElementById("staff").checked = true;
-					if(wowClass.two_handed_axe)
-						document.getElementById("two_handed_axe").checked = true;
-					if(wowClass.two_handed_mace)
-						document.getElementById("two_handed_mace").checked = true;
-					if(wowClass.two_handed_sword)
-						document.getElementById("two_handed_sword").checked = true;
-					if(wowClass.bow)
-						document.getElementById("bow").checked = true;
-					if(wowClass.crossbow)
-						document.getElementById("crossbow").checked = true;
-					if(wowClass.gun)
-						document.getElementById("gun").checked = true;
-					if(wowClass.shield)
-						document.getElementById("shield").checked = true;
-					if(wowClass.off_hand_frill)
-						document.getElementById("off_hand_frill").checked = true;
-				}
-			});
+		classes.forEach(wowClass => {
+			if(wowClass.class == strClass) {
+				if(wowClass.cloth)
+					document.getElementById("cloth_armor").checked = true;
+				if(wowClass.leather)
+					document.getElementById("leather_armor").checked = true;
+				if(wowClass.mail)
+					document.getElementById("mail_armor").checked = true;
+				if(wowClass.plate)
+					document.getElementById("plate_armor").checked = true;
+				if(wowClass.agility)
+					document.getElementById("agility").checked = true;
+				if(wowClass.intellect)
+					document.getElementById("intellect").checked = true;
+				if(wowClass.strength)
+					document.getElementById("strength").checked = true;
+				if(wowClass.dagger)
+					document.getElementById("dagger").checked = true;
+				if(wowClass.one_handed_axe)
+					document.getElementById("one_handed_axe").checked = true;
+				if(wowClass.one_handed_mace)
+					document.getElementById("one_handed_mace").checked = true;
+				if(wowClass.one_handed_sword)
+					document.getElementById("one_handed_sword").checked = true;
+				if(wowClass.fist_weapon)
+					document.getElementById("fist_weapon").checked = true;
+				if(wowClass.wand)	
+					document.getElementById("wand").checked = true;
+				if(wowClass.warglaives)
+					document.getElementById("warglaives").checked = true;
+				if(wowClass.polearm)
+					document.getElementById("polearm").checked = true;
+				if(wowClass.staff)
+					document.getElementById("staff").checked = true;
+				if(wowClass.two_handed_axe)
+					document.getElementById("two_handed_axe").checked = true;
+				if(wowClass.two_handed_mace)
+					document.getElementById("two_handed_mace").checked = true;
+				if(wowClass.two_handed_sword)
+					document.getElementById("two_handed_sword").checked = true;
+				if(wowClass.bow)
+					document.getElementById("bow").checked = true;
+				if(wowClass.crossbow)
+					document.getElementById("crossbow").checked = true;
+				if(wowClass.gun)
+					document.getElementById("gun").checked = true;
+				if(wowClass.shield)
+					document.getElementById("shield").checked = true;
+				if(wowClass.off_hand_frill)
+					document.getElementById("off_hand_frill").checked = true;
+			}
 		});
-
-	updateItems();
+		
+		updateItems();
+	});
 }
